fa78a0c026b99205a24696c2657a373d31f34eff
mensonones <mensones.1@gmail.com>
2019-09-27 13:20:08 -0300

    created an example and explaining a component react

---
+++ criando-um-componente-reactjs/start-react/src/components/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "../styles/App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: ""
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      nome: "React"
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>{this.state.nome}</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
